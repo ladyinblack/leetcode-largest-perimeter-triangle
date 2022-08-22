7b20b009c3ba35f8b802973fc9bf169852b05432
Ronica Singh <ladyinblack2010rs@gmail.com>
2022-08-22 11:26:48 +0200

    Create LargestPerimeter.cs

---
+++ LargestPerimeter.cs
@@ -0,0 +1,9 @@
+public int LargestPerimeter(int[] nums) {
+    Array.Sort(nums);
+    
+    for (int i = nums.Length - 1; i >= 2; i--)
+        if (nums[i] < nums[i - 1] + nums[i - 2])
+            return nums[i] + nums[i - 2] + nums[i - 1];
+
+    return 0;
+}
